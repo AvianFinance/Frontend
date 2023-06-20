ab8bc2e68875f8f986c5dfd5c8422adb2057d5cc
Isuru Ariyarathne <61895498+IsuruAriyarathne@users.noreply.github.com>
2023-06-20 22:20:27 +0530

    Update contract

--- utils/contracts.js
+++ utils/contracts.js
@@ -1,9 +1,9 @@
 // let selxchange_token = "0xDdd526e1F50C8842CD1Bd59c0e05Aa4F35Ee2fc5"
 // let rexchange_token = "0xE7e9137732c4eE0f6287a3b6356B48C10542c066"
 // let iexchange_token = "0xFf0867165415bF58dC3498a521761Db8e77B281c"
-let selxchange_token = "0xfa66f3fe05a3DDf13019FFBc9C7F358c18106c1A"
-let rexchange_token = "0xf601bE828BD57a0fe8458969e4C71640Ca6F762c"
-let iexchange_token = "0xfa66f3fe05a3DDf13019FFBc9C7F358c18106c1A"
+let selxchange_token = "0xed778788773BeA7ea61635d33596A6dFc4a86900"
+let rexchange_token = "0xA5cDcBdeb08D4126D57c42A573Dc39958E0eCB19"
+let iexchange_token = "0xF2350c531812ED5352372673a83d451A7463C43b"
 let rime_token   = "0x4909493F604AB882327ca880ad5B330e2B3C43C1"
 let rime_rent = "0xA5e80F4980878b7C2c23D6fA002358A47d0060a3"
 
